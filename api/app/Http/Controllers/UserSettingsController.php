e38d84105424d887ad6523e2dd5db8d088dc3fd5
Maciej Ładoś <maciej@lados.dev>
2020-11-30 19:30:33 +0100

    docs: user settings api routes

---
+++ api/app/Http/Controllers/UserSettingsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserSettingsController extends Controller
+{
+    /**
+     * Create or overwrite user settings
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display user settings
+     *
+     * @param  int  $userId
+     * @return \Illuminate\Http\Response
+     */
+    public function show($userId)
+    {
+        //
+    }
+
+    /**
+     * Update specified user settings
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $userId
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $userId)
+    {
+        //
+    }
+
+    /**
+     * Delete specified user settings
+     *
+     * @param  int  $userId
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($userId)
+    {
+        //
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -13,7 +13,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\UserSettingsController;
 use App\Http\Controllers\WalletController;
 
+Route::resource('/user/settings', UserSettingsController::class);
 Route::resource('/game', GameController::class);
 Route::resource('/wallet', WalletController::class);
