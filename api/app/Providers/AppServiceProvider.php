7b5d3458bf942c8c81d7706d52f0eb91e0664d9b
Maciej Ładoś <maciej@lados.dev>
2020-11-30 21:34:34 +0100

    ci: api docs deployment

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (Str::startsWith(env('APP_URL'), 'https')) {
+            URL::forceScheme('https');
+        }
     }
 }
