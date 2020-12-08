05c750d778d24bce742d992623f1e0acd5999f99
Maciej Ładoś <macieklad@gmail.com>
2020-12-08 18:43:11 +0100

    feat: authentication test helpers

--- api/tests/Helpers.php
+++ api/tests/Helpers.php
@@ -2,10 +2,37 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 /**
- * A basic assert example.
+ * Get email and password for a valid, newly created user
  */
-function assertExample(): void
+function validAuthCreds(): array
 {
-    test()->assertTrue(true);
+    return [
+        'email' => User::factory()->create()->email,
+        'password' => 'password'
+    ];
+}
+
+/**
+ * Retrieve valid auth jwt token from current session
+ */
+function grabAuthToken(): string
+{
+    return Auth::attempt(validAuthCreds());
+}
+
+/**
+ * Launch new test as authenticated user
+ */
+function withAuthHeader($token = null)
+{
+    if (!$token)
+        $token = grabAuthToken();
+
+    return test()->withHeaders([
+        'Authorization' => "Bearer $token"
+    ]);
 }
