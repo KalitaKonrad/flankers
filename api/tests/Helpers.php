90d7056340baa03193d91c08cc27880711e879db
Maciej Ładoś <macieklad@gmail.com>
2020-12-08 20:46:40 +0100

    test: unverfied user and default password helpers

--- api/tests/Helpers.php
+++ api/tests/Helpers.php
@@ -6,14 +6,24 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 /**
+ * Return array with default user password
+ */
+function withDefaultPassword(array $arr)
+{
+    return array_merge(
+        $arr,
+        ['password' => 'password']
+    );
+}
+
+/**
  * Get email and password for a valid, newly created user
  */
 function validAuthCreds(): array
 {
-    return [
-        'email' => User::factory()->create()->email,
-        'password' => 'password'
-    ];
+    return withDefaultPassword([
+        'email' => User::factory()->create()->email
+    ]);
 }
 
 /**
@@ -35,4 +45,17 @@ function withAuthHeader($token = null)
     return test()->withHeaders([
         'Authorization' => "Bearer $token"
     ]);
+}
+
+/**
+ * Return fresh, unverified user
+ */
+function unverifiedUser(): User
+{
+    $creds = validAuthCreds();
+    $user = User::where('email', $creds['email'])->first();
+    $user->email_verified_at = null;
+    $user->save();
+
+    return $user;
 }
