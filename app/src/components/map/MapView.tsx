c2a1e0b7a3b5e15d8fbbf63efaec2740dc525387
SikoraKam <sikorakam3@gmail.com>
2020-12-09 17:13:29 +0100

    feat: create MapView with heatmap based on cordinates form device

---
+++ app/src/components/map/MapView.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import {
+  Dimensions,
+  View,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import MapView, { Heatmap, Marker, WeightedLatLng } from 'react-native-maps';
+
+interface MapViewProps {
+  heatPoints: WeightedLatLng[];
+  markers: any[];
+}
+
+export const MapViewComponent: React.FC<MapViewProps> = (props) => {
+  const [mapPoints, setMapPoints] = useState();
+  const [markers, setMarkers] = useState([]);
+
+  return (
+    <>
+      <View style={styles.container}>
+        <MapView
+          initialRegion={{
+            latitude: 50.068607,
+            longitude: 19.90621,
+            latitudeDelta: 0.003,
+            longitudeDelta: 0.003,
+          }}
+          style={styles.mapStyle}>
+          <Heatmap points={props.heatPoints} />
+
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={marker.latlng}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
--- app/src/screens/match/MatchJoinFromMapScreen.tsx
+++ app/src/screens/match/MatchJoinFromMapScreen.tsx
@@ -1,7 +1,8 @@
 import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Text, TouchableOpacity, View } from 'react-native';
 
+import { MapViewComponent } from '../../components/map/MapView';
 import { MatchScreenStackParamList } from './MatchScreenStack';
 
 type MatchJoinFromMapScreenProps = object &
@@ -10,9 +11,31 @@ type MatchJoinFromMapScreenProps = object &
 export const MatchJoinFromMapScreen: React.FC<MatchJoinFromMapScreenProps> = ({
   navigation,
 }) => {
+  const [location, setLocation] = useState<any>(null);
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
+
+  const findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const location1 = JSON.stringify(position);
+
+        setLocation(location1);
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      (error) => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
+
   return (
     <>
-      <Text>Match Join From Map Screen</Text>
+      <TouchableOpacity onPress={findCoordinates}>
+        <Text>Find my cords</Text>
+        <Text>Location: {location}</Text>
+      </TouchableOpacity>
+      <MapViewComponent arr={[{ latitude, longitude, weight: 1 }]} />
     </>
   );
 };
