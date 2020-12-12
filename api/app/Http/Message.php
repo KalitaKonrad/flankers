9f444c7a39df624335ec1c04a9ad65351ab8f2a2
Maciej Ładoś <macieklad@gmail.com>
2020-12-12 18:29:21 +0100

    feat: add error http message helper

--- api/app/Http/Message.php
+++ api/app/Http/Message.php
@@ -10,10 +10,25 @@ class Message
      * @param string $message
      * @return void
      */
-    public static function ok(string $message)
+    public static function ok(string $message, $data = [])
     {
         return response([
-            'message' => __($message)
+            'message' => __($message),
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * Return error response with message
+     *
+     * @param string $message
+     * @return void
+     */
+    public static function error(int $code, string $message, $errors = [])
+    {
+        return response([
+            'message' => __($message),
+            'errors' => $errors
         ]);
     }
 }
