d43f0def4036de1aa5cf9166489b8ed47525d4f9
Maciej Ładoś <macieklad@gmail.com>
2020-12-12 19:50:17 +0100

    feat: invite accept

---
+++ api/app/Http/Controllers/AcceptInvite.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Message;
+use App\Models\User;
+
+class AcceptInvite extends Controller
+{
+    /**
+     * Accept an invite
+     *
+     * @urlParam token string Invite accept token
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(string $token)
+    {
+        $inviteModel = config('teamwork.invite_model');
+        $invite = $inviteModel::where('accept_token', $token)->firstOrFail();
+
+        User::findOrFail($invite->user_id)->attachTeam($invite->team);
+        $invite->delete();
+
+        return Message::ok('Invite accepted');
+    }
+}
