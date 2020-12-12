6d14dc9292f2abdf3634ec83f3df913565254eb5
Maciej Ładoś <macieklad@gmail.com>
2020-12-12 21:50:31 +0100

    feat: team deletion tests

--- api/tests/Feature/TeamTest.php
+++ api/tests/Feature/TeamTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use function Tests\grabAuthToken;
@@ -70,4 +71,63 @@ it('should fail while updating non-existent team', function () {
     withAuthHeader()
         ->putJson('/teams/100', $team)
         ->assertStatus(404);
+});
+
+it('should allow for team owner to delete the team', function () {
+    $team = [
+        'name' => 'flankers',
+        'description' => 'foobar'
+    ];
+    $token = grabAuthToken();
+
+    withAuthHeader($token)
+        ->postJson("/teams", $team)
+        ->assertOk();
+
+    withAuthHeader($token)
+        ->delete('/teams/1')
+        ->assertOk();
+
+    $this->assertDatabaseMissing('teams', $team);
+});
+
+it('should disallow for any other user than owner to delete the team', function () {
+    $team = [
+        'name' => 'flankers',
+        'description' => 'foobar'
+    ];
+
+    withAuthHeader()
+        ->postJson("/teams", $team)
+        ->assertOk();
+
+    withAuthHeader()
+        ->delete('/teams/1')
+        ->assertStatus(403);
+});
+
+it('should remove all users from the team if it was deleted', function () {
+    $team = [
+        'name' => 'flankers',
+        'description' => 'foobar'
+    ];
+    $token = grabAuthToken();
+
+    $res = withAuthHeader($token)
+        ->postJson("/teams", $team)
+        ->assertOk();
+    $teamId = $res['data']['id'];
+
+    User::factory(3)
+        ->create()
+        ->each(function ($user) use ($teamId) {
+            $user->current_team_id = $teamId;
+            $user->save();
+        });
+
+    withAuthHeader($token)
+        ->delete('/teams/1')
+        ->assertOk();
+
+    $this->assertTrue(User::where('current_team_id', $teamId)->count() == 0);
 });
