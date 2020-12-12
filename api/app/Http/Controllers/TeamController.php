f7aefb00e619364b29a38d81d86e6c65ddd0d952
Maciej Ładoś <macieklad@gmail.com>
2020-12-12 17:26:09 +0100

    feat: setup team controller from generated boilerplate

--- api/app/Http/Controllers/TeamController.php
+++ api/app/Http/Controllers/TeamController.php
@@ -1,63 +1,117 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Teamwork;
 
+use App\Http\Message;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Mpociot\Teamwork\Exceptions\UserNotInTeamException;
 
 class TeamController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /**
-     * List existing teams
+     * Display list o fuser teams
+     *
+     * @group Team management
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return Auth::user()->teams;
     }
 
     /**
-     * Create new team
+     * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $teamModel = config('teamwork.team_model');
+
+        $team = $teamModel::create([
+            'name' => $request->name,
+            'owner_id' => $request->user()->getKey(),
+        ]);
+        $request->user()->attachTeam($team);
+
+        return redirect(route('teams.index'));
     }
 
     /**
-     * Display details about a team
+     * Switch to the given team.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function switchTeam($id)
     {
-        //
+        $teamModel = config('teamwork.team_model');
+        $team = $teamModel::findOrFail($id);
+        try {
+            Auth::user()->switchTeam($team);
+        } catch (UserNotInTeamException $e) {
+            abort(403);
+        }
+
+        return  Message::ok('Team switched successfully');
     }
 
     /**
-     * Update team information
+     * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $teamModel = config('teamwork.team_model');
+
+        $team = $teamModel::findOrFail($id);
+        $team->name = $request->name;
+        $team->save();
+
+        return redirect(route('teams.index'));
     }
 
     /**
-     * Delete a team
+     * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        $teamModel = config('teamwork.team_model');
+
+        $team = $teamModel::findOrFail($id);
+        if (!Auth::user()->isOwnerOfTeam($team)) {
+            abort(403);
+        }
+
+        $team->delete();
+
+        $userModel = config('teamwork.user_model');
+        $userModel::where('current_team_id', $id)
+            ->update(['current_team_id' => null]);
+
+        return redirect(route('teams.index'));
     }
 }
