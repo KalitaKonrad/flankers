9186987880c8433dae4cbccab33ea340beca7076
Maciej Ładoś <macieklad@gmail.com>
2020-12-12 22:35:32 +0100

    chore: team model factory

---
+++ api/database/factories/TeamFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TeamFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Team::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
+            'owner_id' => User::factory()->create()->id
+        ];
+    }
+
+    /**
+     * Configure the model factory.
+     *
+     * @return $this
+     */
+    public function configure()
+    {
+        return $this->afterCreating(function ($team) {
+            User::find($team->owner_id)->attachTeam($team);
+        });
+    }
+}
