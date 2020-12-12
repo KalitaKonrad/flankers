aebb1018391dcdb594a4de1e6e2faa088b937e8c
Maciej Ładoś <macieklad@gmail.com>
2020-12-12 19:38:36 +0100

    chore: add TeamMember trait to user

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,18 +2,18 @@
 
 namespace App\Models;
 
+use App\Traits\TeamMember;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Mpociot\Teamwork\Traits\UserHasTeams;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
-use Laravel\Sanctum\HasApiTokens;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable, UserHasTeams;
+    use HasApiTokens, HasFactory, Notifiable, TeamMember;
 
     /**
      * The attributes that are mass assignable.
@@ -67,7 +67,7 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 
     /**
      * Hash user password when updating it
-     * 
+     *
      * @return void
      */
     public function setPasswordAttribute($password)
