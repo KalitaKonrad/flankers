c769c127001c8d583819fa0afc000d73170e9d05
Maciej Ładoś <macieklad@gmail.com>
2020-12-13 16:21:44 +0100

    feat: game and squad model relations and methods

--- api/app/Models/Game.php
+++ api/app/Models/Game.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Game extends Model
 {
     use HasFactory;
+
+    /**
+     * Return squads registered to the game
+     *
+     * @return HasMany
+     */
+    public function squads()
+    {
+        return $this->hasMany(Squad::class);
+    }
+
+    /**
+     * Return players registered to the game
+     *
+     * @return Collection
+     */
+    public function players()
+    {
+        return $this->squads()->with('members')->get()->pluck('members')->flatten();
+    }
+
+    /**
+     * Return player memos related to the class
+     *
+     * @return HasMany
+     */
+    public function memos()
+    {
+        return $this->hasMany(Memo::class);
+    }
 }
