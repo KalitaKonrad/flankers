a5d51c0ee76f886e2bb472a7c63fca87a94d2115
Maciej Ładoś <macieklad@gmail.com>
2020-12-13 17:19:35 +0100

    feat: request for game creation

---
+++ api/app/Http/Requests/CreateGameRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateGameRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'type' => 'in:public,private',
+            'rated' => 'boolean',
+            'public' => 'boolean',
+            'bet' => 'numeric',
+            'duration' => 'integer',
+            'long' => 'between:-180:180|nullable',
+            'lat' => 'between:-90,90|nullable'
+        ];
+    }
+}
