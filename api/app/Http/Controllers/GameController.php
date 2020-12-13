478b9ca11146f740f9a0e3274be6124e17eb46f9
Maciej Ładoś <macieklad@gmail.com>
2020-12-13 17:19:49 +0100

    feat: game creation endpoint

--- api/app/Http/Controllers/GameController.php
+++ api/app/Http/Controllers/GameController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
+use App\Http\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\CreateGameRequest;
 
 class GameController extends Controller
 {
     /**
+     * Instantiate the controller
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
      * Show list of games
+     *
+     * @group Game data
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return Game::all();
     }
 
     /**
@@ -22,9 +36,21 @@ class GameController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateGameRequest $request)
     {
-        //
+        $defaults = [
+            'owner_id' => Auth::id(),
+            'type' => 'public',
+            'rated' => false,
+            'public' => true,
+            'bet' => 0,
+            'duration' => 60 * 10,
+            'long' => null,
+            'lat' => null
+        ];
+        $data = array_merge($defaults, $request->all());
+
+        return Message::ok('Game created', Game::create($data));
     }
 
     /**
