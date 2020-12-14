7660ca3db47055373e00620eafab92403ac01204
Maciej Ładoś <macieklad@gmail.com>
2020-12-14 23:44:45 +0100

    feat: game management with docs

--- api/app/Http/Controllers/Game/GameController.php
+++ api/app/Http/Controllers/Game/GameController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Game;
 
 use App\Models\Game;
 use App\Http\Message;
+use App\Models\Squad;
+use Illuminate\Support\Carbon;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\CreateGameRequest;
@@ -21,6 +23,10 @@ class GameController extends Controller
 
     /**
      * Show list of games
+     *
+     * Return list of all games, but without details,
+     * fetch specified game from here to get more
+     * informations.
      *
      * @group Game data
      *
@@ -63,12 +69,23 @@ class GameController extends Controller
             'lat' => null
         ];
         $data = array_merge($defaults, $request->all());
+        $game = Game::create($data);
 
-        return Message::ok('Game created', Game::create($data));
+        Squad::factory(2)
+            ->state([
+                'game_id' => $game->id,
+                'slots' => 5
+            ])
+            ->create();
+
+        return Message::ok('Game created', $game->with('squads')->find($game->id));
     }
 
     /**
      * Display details of a game
+     *
+     * Get all game details with squads
+     * and members.
      *
      * @group Game data
      * @urlParam id int required Game id
@@ -76,20 +93,27 @@ class GameController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(int $game_id)
     {
-        return Game::findOrFail($id)->with('squads');
+        return Game::with('squads', 'squads.members')->findOrFail($game_id);
     }
 
     /**
      * Update the specified game.
+     *
+     * You can update all game fields but the process will fail if:
+     * - game was already marked as complete
+     * - you will set start_date in the past
+     * - if you try to update game bets or rating type
+     *   after it has started (start_date < now)
+     * These failures return 406 code
      *
      * @group Game management
      * @bodyParam type string Public or private
      * @bodyParam rated boolean If ranking points should be assigned rated
      * @bodyParam bet integer Game bet which each user will be charged for
      * @bodyParam duration integer Game time in seconds, default
-     * @bodyParam start_date timestamp Game starting time
+     * @bodyParam start_date timestamp Unix timestamp for game starting time
      * @bodyParam completed boolean True if game should be finalized instantly
      * @bodyParam long Game longitude - default null
      * @bodyParam lat Game latitude - default null
@@ -101,12 +125,17 @@ class GameController extends Controller
     public function update(UpdateGameRequest $request, $id)
     {
         $game = Game::findOrFail($id);
+        $now = Carbon::now()->timestamp;
 
         if ($game->completed) {
             return Message::error(406, 'This game was already completed, you cannot change it');
         }
 
-        if ($game->start_date && ($request->rated || $request->bet)) {
+        if ($request->start_date && $game->start_date < $now) {
+            return Message::error(406, 'Cannot start game in the past');
+        }
+
+        if ($game->start_date && $game->start_date < $now && ($request->rated || $request->bet)) {
             return Message::error(406, 'You cannot change game bets or rating type if it has already started');
         }
 
@@ -118,6 +147,9 @@ class GameController extends Controller
 
     /**
      * Delete the game
+     *
+     * This will fail with 406 code if the
+     * game was already completed.
      *
      * @group Game management
      * @urlParam id int required Game id
