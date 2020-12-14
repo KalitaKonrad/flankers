5c9a33e2cc54d8e32a83d45c5870f700edfa09a8
Maciej Ładoś <macieklad@gmail.com>
2020-12-14 23:43:33 +0100

    docs: update route paths,  param names and controller scopes

--- api/routes/api.php
+++ api/routes/api.php
@@ -24,11 +24,21 @@ use App\Http\Controllers\ForgotPassword;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ChangeTeamOwner;
+use App\Http\Controllers\SquadController;
+use App\Http\Controllers\GameMemoController;
+use App\Http\Controllers\Game\GameController;
 use App\Http\Controllers\TeamInviteController;
 use App\Http\Controllers\UserAvatarController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\TeamMembershipController;
+use App\Http\Controllers\Game\SquadMembershipController;
 
+
+/**
+ * ----------------------------------------
+ * Auth related routes
+ * ----------------------------------------
+ */
 Route::prefix('auth')->group(function () {
     Route::post('signin', Signin::class);
     Route::post('signout', Signout::class);
@@ -42,6 +52,12 @@ Route::prefix('auth')->group(function () {
     Route::post('reset-password', ResetPassword::class);
 });
 
+
+/**
+ * ----------------------------------------
+ * User management routes
+ * ----------------------------------------
+ */
 Route::get('user', [UserController::class, 'index']);
 Route::prefix('user')->group(function () {
     Route::get('settings', [UserProfileController::class, 'show']);
@@ -49,9 +65,14 @@ Route::prefix('user')->group(function () {
     Route::resource('avatar', UserAvatarController::class);
 });
 
-Route::resource('teams', TeamController::class)->parameters([
-    'teams' => 'team_id'
-]);
+/**
+ * ----------------------------------------
+ * Team routes
+ * ----------------------------------------
+ */
+Route::resource('teams', TeamController::class)
+    ->parameters(['teams' => 'team_id']);
+
 Route::prefix('teams')->group(function () {
     Route::post('owner', ChangeTeamOwner::class);
     Route::resource('memberships', TeamMembershipController::class)->parameters([
@@ -62,13 +83,24 @@ Route::prefix('teams')->group(function () {
     Route::get('invites/decline/{invite}', DeclineInvite::class);
 });
 
-// Route::prefix('games')->group(function () {
-//     Route::resource('', GameController::class);
-//     Route::resource('invites', GameInviteController::class);
-//     Route::resource('bets', GameBetsController::class);
-//     Route::resource('scores', GameScoreController::class);
-//     Route::get('/find', FindGame::class);
-//     Route::post('/join/{game}', JoinGame::class);
-//     Route::post('/settle/{game}', SettleGame::class);
-// });
-// Route::resource('/wallet', WalletController::class);
+/**
+ * ----------------------------------------
+ * Game routes
+ * ----------------------------------------
+ */
+Route::resource('games', GameController::class)
+    ->parameters(['games' => 'game_id'])
+    ->only(['index', 'store', 'show', 'update', 'destroy']);
+
+Route::prefix('games')->group(function () {
+    // Route::resource('invites', GameInviteController::class);
+    // Route::resource('bets', GameBetsController::class);
+
+    Route::resource('memos', GameMemoController::class)
+        ->parameters(['memos' => 'memo_id'])
+        ->only(['store', 'show', 'update', 'destroy']);
+
+    Route::resource('memberships', SquadMembershipController::class)
+        ->parameters(['membership' => 'squad_id'])
+        ->only(['index', 'store', 'show', 'update', 'destroy']);
+});
