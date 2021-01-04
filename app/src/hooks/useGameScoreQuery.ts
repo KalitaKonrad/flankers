cbdbe612829087c41a791ff92ecb9faa9ea35a02
SikoraKam <sikorakam3@gmail.com>
2021-01-04 18:52:54 +0100

    feat: missing queries, squadMembership and GameScore

---
+++ app/src/hooks/useGameScoreQuery.ts
@@ -0,0 +1,15 @@
+import { useQuery } from 'react-query';
+
+import { QUERY_GAMES_MEMOS } from '../const/query.const';
+import { GameScoreResponse } from '../types/gameScoreResponse';
+import { useAxios } from './useAxios';
+
+export const useGameScoreQuery = (gameId: number) => {
+  const axios = useAxios();
+
+  return useQuery<GameScoreResponse>(QUERY_GAMES_MEMOS, async () => {
+    const response = await axios.get(`games/memos/${gameId}`);
+
+    return response.data;
+  });
+};
