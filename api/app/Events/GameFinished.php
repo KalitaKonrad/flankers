29e606ac160c144287afeecbe339688cadeb2f35
Maciej Ładoś <macieklad@gmail.com>
2021-01-05 20:50:37 +0100

    chore: game state events

--- api/app/Events/GameFinished.php
+++ api/app/Events/GameFinished.php
@@ -4,14 +4,14 @@ namespace App\Events;
 
 use App\Models\Game;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
-class GameUpdated implements ShouldBroadcast
+class GameFinished implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- api/app/Events/UserChangedSquad.php
+++ api/app/Events/UserChangedSquad.php
@@ -4,27 +4,29 @@ namespace App\Events;
 
 use App\Models\Game;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
-class GameUpdated implements ShouldBroadcast
+class UserChangedSquad
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $game;
+    protected $game;
+    public $squads;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct(Game $game)
+    public function __construct(Game $game, array $squads)
     {
         $this->game = $game;
+        $this->squads = $squads;
     }
 
     /**
