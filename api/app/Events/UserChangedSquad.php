07d5fab421b7dc3d279b6076131018ae047e3e9a
Maciej Ładoś <macieklad@gmail.com>
2021-01-09 12:58:19 +0100

    chore: remove unused imports

--- api/app/Events/UserChangedSquad.php
+++ api/app/Events/UserChangedSquad.php
@@ -4,12 +4,9 @@ namespace App\Events;
 
 use App\Models\Game;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
 
 class UserChangedSquad
 {
