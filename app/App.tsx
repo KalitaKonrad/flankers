908a84b7aab3b61536d73793293c63c07475e3c1
Konrad Kalita <konradkalita@student.agh.edu.pl>
2021-01-09 13:14:22 +0100

    feat: basic notification setup

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import { Subscription } from '@unimodules/react-native-adapter';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import React, { useEffect, useRef, useState } from 'react';
+import { Platform, Text, View, Button } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { QueryCache, ReactQueryCacheProvider, setConsole } from 'react-query';
 
@@ -15,18 +20,143 @@ setConsole({
 
 const queryCache = new QueryCache();
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 const App: React.FC = () => {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [
+    notification,
+    setNotification,
+  ] = useState<Notifications.Notification>();
+  const notificationListener = useRef<Subscription>();
+  const responseListener = useRef<Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token || '')
+    ); // added || "" here
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log('notification', notification);
+        setNotification(notification);
+      }
+    );
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+
+    return () => {
+      // @ts-ignore
+      Notifications.removeNotificationSubscription(notificationListener);
+      // @ts-ignore
+      Notifications.removeNotificationSubscription(responseListenuseer);
+    };
+  }, []);
+
+  console.log('expo notification', notification);
   return (
     <AuthProvider>
       <ReactQueryCacheProvider queryCache={queryCache}>
         <PaperProvider theme={theme}>
           <NavigationContainer>
-            <AppScreen />
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'space-around',
+              }}>
+              <Text>Your expo push token: {expoPushToken}</Text>
+              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Text>
+                  Title: {notification && notification.request.content.title}{' '}
+                </Text>
+                <Text>
+                  Body: {notification && notification.request.content.body}
+                </Text>
+                <Text>
+                  Data:{' '}
+                  {notification &&
+                    JSON.stringify(notification.request.content.data)}
+                </Text>
+              </View>
+              <Button
+                title="Press to Send Notification"
+                onPress={async () => {
+                  await sendPushNotification(expoPushToken);
+                }}
+              />
+            </View>
+            {/*<AppScreen />*/}
           </NavigationContainer>
         </PaperProvider>
       </ReactQueryCacheProvider>
     </AuthProvider>
   );
 };
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/notifications
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { data: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 
 export default App;
