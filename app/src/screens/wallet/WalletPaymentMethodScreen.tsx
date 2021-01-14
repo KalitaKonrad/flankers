97687764c549ff494a7bb6b508a603256689e400
Michał Bar <logout400@gmail.com>
2021-01-15 00:54:52 +0100

    feat: create payment method using stripe api

--- app/src/screens/wallet/WalletPaymentMethodScreen.tsx
+++ app/src/screens/wallet/WalletPaymentMethodScreen.tsx
@@ -1,5 +1,5 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 // @ts-ignore
@@ -8,6 +8,7 @@ import { CreditCardInput } from 'react-native-credit-card-input';
 import { Container } from '../../components/layout/Container';
 import { PaddedInputScrollView } from '../../components/layout/PaddedInputScrollView';
 import { AppButton } from '../../components/shared/AppButton';
+import { Stripe } from '../../lib/stripe';
 import { CreditCardInputResult } from '../../types/credit-card-input-result';
 import { WalletScreenStackParamList } from './WalletScreenStack';
 
@@ -17,15 +18,46 @@ type WalletPaymentMethodScreenProps = StackScreenProps<
 >;
 
 export const WalletPaymentMethodScreen: React.FC<WalletPaymentMethodScreenProps> = () => {
+  const [card, setCard] = useState<CreditCardInputResult | null>(null);
+
+  const onSave = async () => {
+    if (!card || !card.valid) {
+      alert('Wprowadź prawidłowe dane');
+      return;
+    }
+
+    const { number, expiry, cvc } = card.values;
+    const [exp_month, exp_year] = expiry.split('/');
+
+    try {
+      const response = await Stripe.createPaymentMethod({
+        type: 'card',
+        card: {
+          number,
+          cvc,
+          exp_month,
+          exp_year,
+        },
+      });
+      const { id } = response.data;
+    } catch (error) {
+      alert(
+        'Podczas zapisywania metody płatności wystąpił błąd. Spróbuj ponownie lub skontaktuj się z supportem.'
+      );
+    }
+  };
+
   return (
     <Container>
       <PaddedInputScrollView>
         <View style={styles.cardContainer}>
           <CreditCardInput
-            onChange={(data: CreditCardInputResult) => console.log(data)}
+            onChange={(data: CreditCardInputResult) => setCard(data)}
           />
         </View>
-        <AppButton mode="contained">Zapisz metodę płatności</AppButton>
+        <AppButton mode="contained" onPress={onSave}>
+          Zapisz metodę płatności
+        </AppButton>
       </PaddedInputScrollView>
     </Container>
   );
---
+++ app/src/types/stripe/create-payment-method-payload.ts
@@ -0,0 +1,9 @@
+interface CreatePaymentMethodPayload {
+  type: string;
+  card: {
+    number: string;
+    exp_month: string;
+    exp_year: string;
+    cvc: string;
+  };
+}
