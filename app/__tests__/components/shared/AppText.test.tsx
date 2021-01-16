bb27ed31efb5b067a0a0a4843c3d7fb322255ad2
Michał Bar <logout400@gmail.com>
2021-01-17 00:36:50 +0100

    feat: test setup

---
+++ app/__tests__/components/shared/AppText.test.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { render } from '@testing-library/react-native';
+//
+// import { AppText } from '../../../src/components/shared/AppText';
+//
+// describe('<AppText>', () => {
+//   it('should render correctly', () => {
+//     const tree = render(<AppText>Test</AppText>).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
