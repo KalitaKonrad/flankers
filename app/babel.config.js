5c5a7785012ea0fabe4921af24a0eb6f8c058bdf
Michał Bar <logout400@gmail.com>
2021-01-17 02:01:52 +0100

    fix: render tests now run

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,23 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-env'],
-    plugins: [
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
+    presets: [
       [
-        'module-resolver',
+        '@babel/preset-react',
         {
-          extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx',
-            '.android.js',
-            '.android.tsx',
-            '.ios.js',
-            '.ios.tsx',
-          ],
-          root: ['./src'],
+          runtime: 'automatic',
         },
       ],
     ],
