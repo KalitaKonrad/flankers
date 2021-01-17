03c6ce0cb2aa260d85d3785782f36a3ac5550a08
Michał Bar <logout400@gmail.com>
2021-01-17 02:28:41 +0100

    feat: AppText tests

--- app/__tests__/components/shared/AppText.test.tsx
+++ app/__tests__/components/shared/AppText.test.tsx
@@ -8,4 +8,15 @@ describe('<AppText>', () => {
     const tree = render(<AppText>Test</AppText>).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('should pass children to inner Text component', () => {
+    const { getByTestId } = render(<AppText>Test text</AppText>);
+    expect(getByTestId('text').props.children).toBe('Test text');
+  });
+
+  it('should set correct styles based on variant', () => {
+    expect(render(<AppText variant="h1">H1</AppText>)).toMatchSnapshot();
+    expect(render(<AppText variant="h2">H1</AppText>)).toMatchSnapshot();
+    expect(render(<AppText variant="h3">H1</AppText>)).toMatchSnapshot();
+  });
 });
