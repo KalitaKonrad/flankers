c9fbf888b285d8b984c1dd086c4c492f060c46fb
Michał Bar <logout400@gmail.com>
2021-01-17 14:14:54 +0100

    fix: babel compatibility issues

--- app/babel.config.js
+++ app/babel.config.js
@@ -2,12 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      [
-        '@babel/preset-react',
-        {
-          runtime: 'automatic',
-        },
-      ],
+      'babel-preset-expo',
+      // [
+      //   '@babel/preset-react',
+      //   {
+      //     runtime: 'automatic',
+      //   },
+      // ],
     ],
   };
 };
