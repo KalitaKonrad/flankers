83e2d6d4bc4f8ca5069d49a846bcd01b971b0d6e
Michał Bar <logout400@gmail.com>
2021-01-18 22:32:39 +0100

    test: AppText tests

--- app/src/components/shared/AppText.tsx
+++ app/src/components/shared/AppText.tsx
@@ -1,4 +1,5 @@
-import React, { ComponentProps } from 'react';
+import * as React from 'react';
+import { ComponentProps } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Paragraph } from 'react-native-paper';
 
