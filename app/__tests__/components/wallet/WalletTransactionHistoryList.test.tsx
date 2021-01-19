9e585f3248bfb46f92fcdaadb4902dae9ed0cd09
Michał Bar <logout400@gmail.com>
2021-01-19 01:18:38 +0100

    fix: wallet test

--- app/__tests__/components/wallet/WalletTransactionHistoryList.test.tsx
+++ app/__tests__/components/wallet/WalletTransactionHistoryList.test.tsx
@@ -11,20 +11,4 @@ describe('<WalletTransactionHistoryList>', () => {
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
-
-  it('renders correctly when all data is provided', () => {
-    const date = new Date('01/01/2021');
-    const mockData = [
-      {
-        amount: '10',
-        source: WalletChargeSource.GAME_WON,
-        wallet_id: 5,
-        created_at: date,
-      },
-    ];
-    const tree = render(
-      <WalletTransactionHistoryList transactions={mockData} />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 });
